7bdb960e9f37c3706fb595d91af54a59d431728a
Karad Nagnath Vishnu <karadnagnath2007@gmail.com>
2026-06-06 11:48:15 +0530

    changed the astro file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,10 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import node from '@astrojs/node';
 import vercel from '@astrojs/vercel/serverless';
-
-// Use Node adapter locally, Vercel adapter in production
-const adapter = process.env.VERCEL
-  ? vercel()
-  : node({ mode: 'standalone' });
 
 export default defineConfig({
   site: 'https://jobtailor.vercel.app',
   output: 'server',
-  adapter: adapter,
+  adapter: vercel(),
   integrations: [react()],
 });
